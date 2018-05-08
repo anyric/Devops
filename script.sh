b7a9c3556e13dddc3ae9a28e113b9589f339a49e
Anyama Richard <richard@Anyamas-MacBook-Pro.local>
2018-05-08 14:45:02 +0300

    Update port

--- script.sh
+++ script.sh
@@ -55,7 +55,7 @@ sudo bash -c 'cat <<EOF> /etc/nginx/sites-available/default
 server {
         listen 80;
         location / {
-                proxy_pass http://127.0.0.1:5000/;
+                proxy_pass http://127.0.0.1:8000/;
                 proxy_set_header HOST $host;
                 proxy_set_header X-Forwarded-Proto $scheme;
                 proxy_set_header X-Real-IP $remote_addr;
