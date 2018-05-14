4eec413aa6d7b8408fb6120dde4210e69e95bb32
anyric <anyamaronyango@gmail.com>
2018-05-14 10:06:03 +0300

    Update config file

--- wscript.sh
+++ wscript.sh
@@ -73,10 +73,10 @@ server {
         listen 80 default_server;
         listen [::]:80 ipv6only=on;
 
-        server_name anyric.tk www.anyric.tk;
+        server_name ;
 
         location / {
-            proxy_pass http://127.0.0.1:5000;
+            proxy_pass http://127.0.0.1:8000;
             proxy_set_header HOST $host;
             proxy_set_header X-Forwarded-Proto $scheme;
             proxy_set_header X-Real-IP $remote_addr;
