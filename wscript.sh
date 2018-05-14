cb9a42b31d7ecb6e111105864a6718b5d1a7c3e6
anyric <anyamaronyango@gmail.com>
2018-05-14 10:02:32 +0300

    Update config file

--- wscript.sh
+++ wscript.sh
@@ -76,7 +76,7 @@ server {
         server_name anyric.tk www.anyric.tk;
 
         location / {
-            proxy_pass http://127.0.0.1:8000;
+            proxy_pass http://127.0.0.1:5000;
             proxy_set_header HOST $host;
             proxy_set_header X-Forwarded-Proto $scheme;
             proxy_set_header X-Real-IP $remote_addr;
@@ -142,9 +142,9 @@ run(){
     cloneRepo
     setupProjectDependancies
     setupHostIP
-    startNginx
     configureNginx
     configureSSH
+    startNginx
     configureSystemd
     restartNginx
     exportDatabaseUrl
